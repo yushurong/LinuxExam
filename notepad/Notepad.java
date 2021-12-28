25ae2c86246eb672b85ceacafca892bea1882fff
yushurong <1781112927@qq.com>
2021-12-28 17:22:46 +0800

    linuxexam

---
+++ notepad/Notepad.java
@@ -0,0 +1,13 @@
+package Notepad;
+
+
+public class Notepad {
+    int id;
+    String notepadContent;
+    String notepadTime;
+
+    @Override
+    public String toString() {
+        return "Notepad [id=" + id + ", notepadContent=" + notepadContent + ", notepadTime=" + notepadTime + "]";
+    }
+}
